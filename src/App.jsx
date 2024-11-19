37cf14c93ae625e27eb50220a28b15f245070863
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-11-19 09:03:34 -0300

    criando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+import VotingApp from "./components/VotingApp/VotingApp";
+
+function App() {
+  return; // INSIRA O COMPONENTE DA APLICACAO AQUI
+}
+
+export default App;
